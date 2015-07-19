c7d49e0e7a7e0e543cdbaad99ddacb68b834816f
Kai Aldag <trainerkai@yahoo.com>
2015-07-18 22:04:06 -0700

    Commit #2

---
+++ CapchaView.swift
@@ -0,0 +1,13 @@
+//
+//  CapchaView.swift
+//  Capcha
+//
+//  Created by kai don aldag on 2015-07-18.
+//  Copyright (c) 2015 kai.don.aldag. All rights reserved.
+//
+
+import UIKit
+
+class CapchaView: UIView {
+    
+}
